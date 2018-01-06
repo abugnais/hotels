16426c99c6a8332ca56ecd4b2808fbc1b43c78ad
Ahmad Moqanasa <ahmad.moqanasa@gmail.com>
2018-01-07 02:45:17 +0400

    hotels controller separated from the route

---
+++ controllers/hotels.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = function (dal) {
+    return {
+
+        search: async function (req, res, next) {
+            try {
+                let hotels = await dal.hotels.search(req.query);
+
+                res.send(hotels);
+            } catch(err) {
+                next(err);
+            }
+        }
+    }
+};
